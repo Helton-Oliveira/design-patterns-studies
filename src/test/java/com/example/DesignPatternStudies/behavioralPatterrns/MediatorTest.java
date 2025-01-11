d84ff7a1184470b725c2466786cc1ded93a6b2ab
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-01-10 21:54:54 -0300

    feat: implementing second test for the second stage of the project

--- src/test/java/com/example/DesignPatternStudies/behavioralPatterrns/MediatorTest.java
+++ src/test/java/com/example/DesignPatternStudies/behavioralPatterrns/MediatorTest.java
@@ -1,15 +1,18 @@
 package com.example.DesignPatternStudies.behavioralPatterrns;
 
-import com.example.DesignPatternStudies.BehavioralPatterns.Mediator.code.step_01.SpaceShip;
-import com.example.DesignPatternStudies.BehavioralPatterns.Mediator.code.step_01.ConcreteControlCenter;
-import com.example.DesignPatternStudies.BehavioralPatterns.Mediator.code.step_01.ControlCenter;
+import com.example.DesignPatternStudies.BehavioralPatterns.Mediator.code.step_02.ConcreteControlCenter;
+import com.example.DesignPatternStudies.BehavioralPatterns.Mediator.code.step_02.Planet;
+import com.example.DesignPatternStudies.BehavioralPatterns.Mediator.code.step_02.SpaceStation;
+import com.example.DesignPatternStudies.BehavioralPatterns.Mediator.code.step_02.Spaceship;
+import com.example.DesignPatternStudies.BehavioralPatterns.Mediator.code.step_02.command.ConcreteInvoker;
+import com.example.DesignPatternStudies.BehavioralPatterns.Mediator.code.step_02.iterator.ConcreteSpaceshipCollection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MediatorTest {
 
 
-    @Test
+/*    @Test
     @DisplayName("Deve mediar voos das aeronaves")
     void mediatorTest_step01() {
         ControlCenter controlCenter = new ConcreteControlCenter();
@@ -19,5 +22,31 @@ public class MediatorTest {
         explorer07.requestLanding();
         explorer03.requestLanding();
         explorer03.requestTakeoff();
+    }*/
+
+    @Test
+    @DisplayName("Deve mediar voos das naves espaciais step_02")
+    void mediatorTest_step02() {
+        var planet = new Planet(10, 7, 20);
+        var spaceStation = new SpaceStation("ZULU-2", 3, 2, 8);
+        var collection = new ConcreteSpaceshipCollection();
+        var invoker = new ConcreteInvoker();
+        var mediator = new ConcreteControlCenter(spaceStation, planet, collection, invoker);
+        var alpha07 = new Spaceship("APLHA-17", 30, 70, "totalIntegrity", mediator);
+        var beta09 = new Spaceship("BETA-09", 50, 30, "damage", mediator);
+        var gamma22 = new Spaceship("GAMMA-22", 90, 30, "totalIntegrity", mediator);
+        var delta34 = new Spaceship("DELTA-34", 75, 30, "totalIntegrity", mediator);
+        var epsilon45 = new Spaceship("EPSILON-45", 100, 30, "totalIntegrity", mediator);
+        var zeta56 = new Spaceship("ZETA-56", 60, 30, "totalIntegrity", mediator);
+
+        alpha07.requestLandingForRefueling();
+        delta34.requestLandingRecoveryOxygen();
+        epsilon45.requestLandingRecoveryOxygen();
+        zeta56.requestLandingRecoveryOxygen();
+        gamma22.requestLandingRecoveryOxygen();
+        beta09.requestLandingRecoveryOxygen();
+        beta09.requestLandingForRepairs();
+
+        planet.runReport();
     }
 }
