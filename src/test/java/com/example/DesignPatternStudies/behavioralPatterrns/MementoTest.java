eef1f94a32ce23bf40b38db84df4ac7228c96086
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-01-14 15:36:09 -0300

    feat: implementing test of the second step of the banking transactions project

--- src/test/java/com/example/DesignPatternStudies/behavioralPatterrns/MementoTest.java
+++ src/test/java/com/example/DesignPatternStudies/behavioralPatterrns/MementoTest.java
@@ -1,13 +1,14 @@
 package com.example.DesignPatternStudies.behavioralPatterrns;
 
-import com.example.DesignPatternStudies.BehavioralPatterns.Memento.code.step_01.Bank;
-import com.example.DesignPatternStudies.BehavioralPatterns.Memento.code.step_01.ConcreteCheckingAccount;
+import com.example.DesignPatternStudies.BehavioralPatterns.Memento.code.step_02.Bank;
+import com.example.DesignPatternStudies.BehavioralPatterns.Memento.code.step_02.ConcreteCheckingAccount;
+import com.example.DesignPatternStudies.BehavioralPatterns.Memento.code.step_02.chainOfResponsibility.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class MementoTest {
 
-    @Test
+    /*@Test
     @DisplayName("Deve registrar extratos de transação bancária")
     void mementoTest_step01() {
         var account = new ConcreteCheckingAccount("Steve Rogers", 2000.0, 987645093L);
@@ -19,5 +20,20 @@ public class MementoTest {
         bank.ShowLastMonthStatement();
         bank.undoTransaction();
         System.out.println(account.getBalance());
+    }*/
+
+    @Test
+    @DisplayName("Deve registrar extratos de transação bancária se estiver de acordo com as validacoes")
+    void mementoTest_step02() {
+        var account = new ConcreteCheckingAccount(7000.0, "Steve Rogers", 987645093L, "985");
+
+        account.pay("123456789", 250.0);
+        account.makePix("23456", 300.0);
+        account.makeDeposit(100.0);
+        account.showHistoryTransactions();
+
+        account.pay("34434", 4000.0); // transaction de erro
+
+        account.showBalance();
     }
 }
