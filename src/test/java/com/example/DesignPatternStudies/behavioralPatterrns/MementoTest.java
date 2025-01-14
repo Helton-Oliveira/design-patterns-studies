554de900d06498d3d9c7fdf98723cd9759b912e1
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-01-13 22:16:21 -0300

    feat: implementing test to execute project

---
+++ src/test/java/com/example/DesignPatternStudies/behavioralPatterrns/MementoTest.java
@@ -0,0 +1,23 @@
+package com.example.DesignPatternStudies.behavioralPatterrns;
+
+import com.example.DesignPatternStudies.BehavioralPatterns.Memento.code.step_01.Bank;
+import com.example.DesignPatternStudies.BehavioralPatterns.Memento.code.step_01.ConcreteCheckingAccount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MementoTest {
+
+    @Test
+    @DisplayName("Deve registrar extratos de transação bancária")
+    void mementoTest_step01() {
+        var account = new ConcreteCheckingAccount("Steve Rogers", 2000.0, 987645093L);
+        var bank = new Bank(account);
+
+        account.makePix(500);
+        bank.backup();
+
+        bank.undoTransaction();
+        bank.ShowLastMonthStatement();
+        System.out.println(account.getBalance());
+    }
+}
