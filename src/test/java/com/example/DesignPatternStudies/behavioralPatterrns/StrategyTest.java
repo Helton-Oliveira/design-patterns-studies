9d53d0f8771b7da03a65f66105757ac7315dcd81
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-01-23 21:19:24 -0300

    feat: implementing testing of the second stage of the transport tariff project

--- src/test/java/com/example/DesignPatternStudies/behavioralPatterrns/StrategyTest.java
+++ src/test/java/com/example/DesignPatternStudies/behavioralPatterrns/StrategyTest.java
@@ -1,13 +1,13 @@
 package com.example.DesignPatternStudies.behavioralPatterrns;
 
-import com.example.DesignPatternStudies.BehavioralPatterns.Strategy.code.step_01.App;
-import com.example.DesignPatternStudies.BehavioralPatterns.Strategy.code.step_01.Transport;
+import com.example.DesignPatternStudies.BehavioralPatterns.Strategy.code.step_02.*;
+import com.example.DesignPatternStudies.BehavioralPatterns.Strategy.code.step_02.Observer.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class StrategyTest {
 
-    @Test
+   /* @Test
     @DisplayName("Deve anunciar os valores das viagens para o usuário")
     void strategyStep_01() {
         var transport = new Transport();
@@ -16,5 +16,31 @@ public class StrategyTest {
         app.requestBusPrice(10.0);
         app.requestMetroValue(10.0);
         app.requestTaxiFare(10.0);
+    }*/
+
+    @Test
+    @DisplayName("Deve anunciar os valores das viagens para o usuário, avisar a troca do valor pelo clima, e alterar os estados dos transportes")
+    void strategyStep_02() {
+        var transport = new TransportStrategy();
+        var pedro = new ConcreteUser("Pedro");
+        var manager = new EventManager();
+        manager.subscribe(pedro.getName(), pedro);
+        var taxi = new Taxi(manager, pedro.getName());
+        var bus = new Bus();
+        var subway = new Subway();
+
+        transport.setTransport(taxi);
+        taxi.underMaintenance();
+        taxi.available();
+        taxi.setClimate("rainy");
+        transport.executeStrategy(13.0);
+
+        transport.setTransport(bus);
+        bus.underMaintenance();
+        transport.executeStrategy(13.0);
+
+        transport.setTransport(subway);
+        subway.available();
+        transport.executeStrategy(13.0);
     }
 }
